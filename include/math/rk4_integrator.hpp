d66851f47ef853a6f54249cbde1bc3f970caa22f
jasonstaker <jason.staker@yahoo.com>
2026-06-04 14:43:38 -0700

    chore: various changes to gitignore, comments, and unecessary includes

--- include/math/rk4_integrator.hpp
+++ include/math/rk4_integrator.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+// the reason I employ RK4 over something like euler is because RK4 uses
+// weighted averages across 4 slopes compared to euler's 1, making it much more
+// numerically accurate and stable, especially since the task demands for it
+
 namespace math {
 
 // Runge-Kutta numerical integration step for generic state vectors and
@@ -11,7 +15,7 @@ State rk4Step(const State& state, double t, double dt, DerivativeFunc f) {
   State k3 = f(state + k2 * (dt / 2.0), t + (dt / 2.0));
   State k4 = f(state + k3 * dt, t + dt);
 
-  return state + (k1 + 2 * k2 + 2 * k3 + k4) * (dt / 6.0);
+  return state + (k1 + 2.0 * k2 + 2.0 * k3 + k4) * (dt / 6.0);
 }
 
 }  // namespace math
